f97127c578d15b1f4181e6041abfad4005e73f85
Alex Lourinha <alex.lourinha@gmail.com>
2022-11-17 11:51:18 +0000

    initial setup

---
+++ src/main/java/com/alexlourinha/planmydate/PlanmydateApplication.java
@@ -0,0 +1,15 @@
+package com.alexlourinha.planmydate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Plan my Date application.
+ */
+@SpringBootApplication
+public class PlanmydateApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(PlanmydateApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/alexlourinha/planmydate/PlanmydateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexlourinha.planmydate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanmydateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
